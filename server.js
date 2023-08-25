783260b2f165fe8323efd306e7bdebe0fd4b8611
Andres Bermudez <afbg.art@gmail.com>
2023-08-25 12:08:59 +0200

    changing cors header to deploy mode

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://sen-rad.github.io');
     next();
 });
 
